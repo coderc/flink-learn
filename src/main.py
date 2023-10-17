7ffce7b6167a1314262db512eb9dc35a537f8657
coderc <shao.c.coding@gmail.com>
2023-10-17 18:58:09 +0800

    [update] check consumer

--- src/main.py
+++ src/main.py
@@ -2,29 +2,31 @@ from pyflink.table import EnvironmentSettings
 from pyflink.common import configuration
 from pkg.environment import envSingleton
 from pkg.config_parser import confSingleton
+from pkg.kafka.consumer import Consumer as KafkaConsumer
 
 def init():
-    print(envSingleton)
     kafka_config = confSingleton.yaml2Dict(envSingleton.getConfPath('kafka_config.yaml'))
-    print(kafka_config)
+    consumer = KafkaConsumer(kafka_config)
+    for con in consumer.getConsumer():
+        print (con)
 
 def main():
     init()
 
-def f1():
-    config = configuration.Configuration()
-    config.set_string("parallelism.default", "128") \
-        .set_string("pipeline.auto-watermark-interval", "800ms") \
-        .set_string("execution.checkpointing.interval", "30s")
+# def f1():
+#     config = configuration.Configuration()
+#     config.set_string("parallelism.default", "128") \
+#         .set_string("pipeline.auto-watermark-interval", "800ms") \
+#         .set_string("execution.checkpointing.interval", "30s")
 
-    env_setting = EnvironmentSettings.new_instance() \
-        .in_streaming_mode() \
-        .with_built_in_catalog_name("my_catalog") \
-        .with_built_in_database_name("my_database") \
-        .with_configuration(config) \
-        .build()
+#     env_setting = EnvironmentSettings.new_instance() \
+#         .in_streaming_mode() \
+#         .with_built_in_catalog_name("my_catalog") \
+#         .with_built_in_database_name("my_database") \
+#         .with_configuration(config) \
+#         .build()
 
-    print(env_setting.get_configuration())
+#     print(env_setting.get_configuration())
 
 
 
--- src/pkg/kafka/consumer.py
+++ src/pkg/kafka/consumer.py
@@ -1,26 +1,20 @@
 import time
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer("my-topic", group_id='my-consumer-group_id-0',bootstrap_servers=['localhost:9093'])
-
-for msg in consumer:
-    print (msg)
-
-
 class Consumer:
     config = {}
     consumer_id = ""
-    consumer
+    consumer = {}
     
     def __init__(self, config):
         self.setup(config)
 
     def setup(self, config):
         self.config = config
-        self.consumer_id = self.config.topic.name + time.time_ns()
-        self.consumer = KafkaConsumer(self.config.topic.name,
+        self.consumer_id = self.config['topic']['name'] + str(time.time_ns())
+        self.consumer = KafkaConsumer(self.config['topic']['name'],
             group_id='group_id' + self.consumer_id,
-            bootstrap_servers = self.config.bootstrap_servers)
+            bootstrap_servers = self.config['bootstrap_servers'])
     
     def getConsumer(self) -> consumer:
         return self.consumer
